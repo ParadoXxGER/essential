73230f1751104dbba1b01ef57aee415eaefcc4fb
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-10-29 17:31:42 +0100

    add env variables

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'pg'
 # Use HAML
 gem 'haml-rails', '~> 1.0'
 # Install reactjs
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  require_relative 'env'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
