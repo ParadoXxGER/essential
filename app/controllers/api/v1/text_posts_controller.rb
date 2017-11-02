d5fa4800aa0e22ae47e17bf8ba5afa0c1ffe73ab
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-11-02 14:41:26 +0100

    use correct json nesting

--- app/controllers/api/v1/text_posts_controller.rb
+++ app/controllers/api/v1/text_posts_controller.rb
@@ -4,9 +4,18 @@ module Api::V1
     # GET /text_posts
     # GET /text_posts.json
     def index
-
-      @posts = TextPost.includes(:user, :comments).references(:user, :comments).select(:created_at, :content, :slug)
-      render json: @posts.to_json(:include => { :user => { :only => [:username, :id, :firstname, :lastname] }, :comments => {:only => [:id, :text], :include => {:user => { only: [:username, :id, :firstname, :lastname]}}} })
+      @posts = TextPost.includes(:user, :comments).references(:user, :comments)
+      render json: @posts.to_json(
+        include: {
+          user: {
+            only: [:username, :id, :firstname, :lastname]
+          },
+          comments: {
+            include: {
+              user: { only: [:username, :id, :firstname, :lastname]}
+            }
+          }
+        })
     end
 
   end
