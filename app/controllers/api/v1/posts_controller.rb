60cd306f7966bd0a4dd09fb186a12295e146f189
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-11-15 19:18:55 +0100

    Add direct post initialization

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,25 +9,18 @@ module Api::V1
       post.user = User.first
       post.content = params[:text]
 
-      if post.save
-        add_filter_to(post)
-        add_tags_to(post)
+      params[:tags].split(' ').each do |tag|
+        post.tags << Tag.new(text: tag, post: post)
       end
+
+      params[:filter].split(' ').each do |filter|
+        post.filter << Filter.new(text: filter, post: post)
+      end
+
+      post.save
     end
 
     private
-
-    def add_tags_to(post)
-      params[:tags].split(' ').each do |tag|
-        Tag.create(text: tag, post: post)
-      end
-    end
-
-    def add_filter_to(post)
-      params[:filter].split(' ').each do |filter|
-        Filter.create(text: filter, post: post)
-      end
-    end
 
     def permit_input
       params.permit(:text, :tags, :filter)
