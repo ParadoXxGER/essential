d686e1fa4b5717ba6c3b6bd99a217528a0cc7854
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-11-18 20:18:26 +0100

    Remove date in docker image

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-DATE=`date +%Y-%m-%d`
-
 set -e
 
 docker-compose up --build --abort-on-container-exit
 
 echo "################ SUCCESS ################"
 
-docker build -t paradoxxger/essential-backend:latest-$DATE ../
+docker build -t paradoxxger/essential-backend:latest ../
 
-docker push paradoxxger/essential-backend:latest-$DATE
+docker push paradoxxger/essential-backend:latest
