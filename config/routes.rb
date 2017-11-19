192dd66dba2f16b5796e9faebf88a529d68cc203
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-11-19 12:52:29 +0100

    fix rubocop and add rubocop config

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
   root 'newsfeed#show'
-
-  namespace :api, { defaults: { format: :json } } do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      # Remember! Do never manually access Parental Class POSTS!!!! Only its childs!
       resources :posts
       resources :newsfeed
     end
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/newsfeed', to: 'newsfeed#show', as: 'newsfeed'
-
-  resources :login, controller: 'user_sessions', only: [:index, :create], as: 'login'
+  resources :login, controller: 'user_sessions', only: %i[index create], as: 'login'
   post '/logout', to: 'user_sessions#destroy'
-
-
 end
+
