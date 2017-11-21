aa260722fa6afaf7d0bf986054cf13e42009078f
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-11-21 08:05:32 +0100

    Remove unused tests

--- test/system/abstract_posts_test.rb
+++
@@ -1,9 +0,0 @@
-require 'application_system_test_case'
-
-class AbstractPostsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit abstract_posts_url
-  #
-  #   assert_selector "h1", text: "AbstractPost"
-  # end
-end
--- test/system/text_posts_test.rb
+++
@@ -1,9 +0,0 @@
-require 'application_system_test_case'
-
-class TextPostsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit text_posts_url
-  #
-  #   assert_selector "h1", text: "TextPost"
-  # end
-end
