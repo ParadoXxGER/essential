4de57c77e7cc55d3363c05dd17135fdc4543dbb8
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-11-21 07:56:55 +0100

    Satisfy rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
     c.crypto_provider = Authlogic::CryptoProviders::BCrypt
     c.validate_email_field = true
     c.validates_format_of_email_field_options = { with: Authlogic::Regex.email_nonascii }
-  end # the configuration block is optional
+  end
 
   before_create :lowercase_username
   before_create :lowercase_email
